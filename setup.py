9aff2407d3575ce7a34590feadd88aff5c41117f
basiczombie <issacgable@gmail.com>
2018-03-19 11:26:46 -0600

    Fixed Version for relese.

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django_python3_saml',
-    version='0.1',
+    version='1.0',
     packages=find_packages(),
     include_package_data=True,
     license='BSD 3-Clause License',  # example license
